a20fa87d12392e3e0ebec899cd8dc350fe480410
blucky8649 <blucky4511@gmail.com>
2022-02-20 22:45:23 +0900

    Livedata 추가

--- app/src/main/java/com/example/viewmodelexample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/viewmodelexample/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.viewmodelexample.R
 import com.example.viewmodelexample.databinding.MainFragmentBinding
 
@@ -32,14 +33,22 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding.resultText.text = viewModel.getResult().toString()
+        //binding.resultText.text = viewModel.getResult().toString()
+
+        // UI 컨트롤러 내부에 옵저버를 생성한다.
+        val resultObserver = Observer<Float> {
+            // 현재의 결과값을 반아 resultText에 저장한다.
+            result -> binding.resultText.text = result.toString()
+        }
+        // 아래 코드를 추가하면 resultObserver의 onChanged() 콜백 함수가 실행되어 resultText값이 바뀐다.
+        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
 
         binding.convertButton.setOnClickListener {
             // 입력창이 비어있지 않다면, 뷰모델의 setAmount 함수를 호출한다.
             if (binding.dollerText.text.isNotEmpty()) {
                 viewModel.setAmount(binding.dollerText.text.toString())
                 // setAmount로 입력된 값을 토대로, 결과값을 뷰모델에 요청한다.
-                binding.resultText.text = viewModel.getResult().toString()
+                // binding.resultText.text = viewModel.getResult().toString()
             } else {
                 // 만약 비어있다면 No Value 라는 문구를 띄운다.
                 binding.resultText.text = "No Value"
--- app/src/main/java/com/example/viewmodelexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodelexample/ui/main/MainViewModel.kt
@@ -1,18 +1,22 @@
 package com.example.viewmodelexample.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val usd_to_eu_rate = 0.74f
     private var dollertext = ""
-    private var result: Float = 0f
+    //private var result: Float = 0f
+    // result 변수를 MutableLiveData 인스턴스에 포함시키고, 이 변숫값의 변경을 관찰하는 옵저버를 생성
+    private var result: MutableLiveData<Float> = MutableLiveData()
 
     fun setAmount(value: String) {
         this.dollertext = value
-        result = value.toFloat() * usd_to_eu_rate
+        // .value 함수를 통해 MutableLiveData에 값을 추가한다.
+        result.value = value.toFloat() * usd_to_eu_rate
     }
-
-    fun getResult(): Float? {
+    // MutablaLivaData 객체를 반환하도록 수정
+    fun getResult(): MutableLiveData<Float> {
         return result
     }
 }
